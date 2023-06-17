5a6428dd49b8d1beea166f0b6703a9945105c093
vasandris01 <undris01@gmail.com>
2023-06-17 10:21:58 +0200

    feladat

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // írd meg a person osztályt
+
+        // példányosísd 3 szor a persont
+
+        // kérd le a statikus fieldet
+
+        // példányosísd még 1 szer
+
+        // kérd le a statikus fieldet
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,7 @@
+public class Person {
+    // írj egy statikus fieldet popular néven
+
+    // írj egy konstruktort ami megnöveli a statikus fieldünket
+
+    // írj egy metódust ami vissza adja a popular értékét
+}
