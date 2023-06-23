cb18baf72c89f7e98c6812d66d46a3dc5c3c33fc
Geree <smithzero94@gmail.com>
2023-06-23 11:35:37 +0200

    Static exercise done

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,23 @@ public class Main {
         // írd meg a person osztályt
 
         // példányosísd 3 szor a persont
+        Person firstPerson = new Person();
+        Person secondPerson = new Person();
+        Person thirdPerson = new Person();
 
         // kérd le a statikus fieldet
+        System.out.println(Person.getPopulation());
 
         // példányosísd még 1 szer
+        Person fourthPerson = new Person();
 
         // kérd le a statikus fieldet
 
+        System.out.println(Person.getPopulation());
+
         // hívd meg a katasztofa metódust
+        Person.catastrophe();
+
+        System.out.println(Person.getPopulation());
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,21 @@
 public class Person {
     // írj egy statikus fieldet popular néven
+    private static int population = 0;
 
     // írj egy konstruktort ami megnöveli a statikus fieldünket
+    public Person() {
+        population++;
+    }
 
     // írj egy metódust ami vissza adja a popular értékét
 
+    public static int getPopulation() {
+        return population;
+    }
+
+
     // írj egy katasztrófa statikus metódust ami nulláza a populart
+    public static void catastrophe() {
+        population = 0;
+    }
 }
